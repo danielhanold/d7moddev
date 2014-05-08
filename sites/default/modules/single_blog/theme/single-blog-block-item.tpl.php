cce507e9260adbef37d0ee19b18dbe48d0645a1a
Daniel Hanold <danny@danielhanold.com>
2014-05-08 10:19:26 -0400

    Create theme to style blog elements in sidebar block

---
+++ sites/default/modules/single_blog/theme/single-blog-block-item.tpl.php
@@ -0,0 +1,11 @@
+<div class="<?php print $classes; ?>">
+  <div class="date"><?php print $date; ?>:</div>
+
+  <h4<?php print $title_attributes; ?>>
+    <?php print render($title); ?>
+  </h4>
+
+  <div class="name">
+    <?php print t('by !username', array('!username' => $name)); ?>
+  </div>
+</div>
